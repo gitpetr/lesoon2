ce5cdc64b05c56fef426a02b5ec9eb4c708072dd
gitpetr <pkhilenko@yandex.ru>
2017-03-23 19:06:28 +0600

    check File.exist?

--- movies.rb
+++ movies.rb
@@ -4,13 +4,19 @@ else
   films = "movies.txt"
 end
 
-allfilms = File.readlines(films)  
-allfilms.map!{|i| i.split("|")}
+if File.exist?(films)
 
-allfilms.each do |film|
-  if film[1].include?("Max")
-    print film[1], " "
-    film[7][-1].to_i.times { print "*" } 
-    puts
+  allfilms = File.readlines(films)  
+  allfilms.map!{|i| i.split("|")}
+
+  allfilms.each do |film|
+    if film[1].include?("Max")
+      print film[1], " "
+      stars = film[7].to_f * 10 - 80
+      print '*' * stars
+      puts
+    end
   end
+else
+  puts "Такого файла не существует"
 end
