9600adb5579d87539f201e2629ae4336ca60532f
gitpetr <pkhilenko@yandex.ru>
2017-03-25 13:55:27 +0600

    update sort by duration

--- lesson3/movies.rb
+++ lesson3/movies.rb
@@ -6,7 +6,8 @@ end
 
 allfilms = File.readlines(films)  
 allfilms.map!{|i| i.split("|")}
- 
+
+puts "5 самых длинных фильмов:"
 allfilms.map do |film| {
                         link: film[0], 
                         name: film[1],
@@ -19,11 +20,12 @@ allfilms.map do |film| {
                         director: film[8], 
                         actors: film[9]
                         }
-              end.sort_by { |hsh| hsh[:duratation] }.reverse[0...5].each_with_index do |viewfilm, c|
-  print "#{c + 1}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]}, #{viewfilm[:actors].chomp}, #{viewfilm[:country]} - #{viewfilm[:duratation]} min). - Link: #{viewfilm[:link]}" 
+              end.sort_by { |hsh| hsh[:duratation].split(" ")[0].to_i }.reverse[0...5].each_with_index do |viewfilm, c|              
+  print "#{c + 1}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]},  #{viewfilm[:genre].split(',').join('/')} - #{viewfilm[:duratation]})." 
   puts
   puts
 end
+puts "10 комедий (первые по дате выхода):"
 
 allfilms.select{ |f| f[5].split(",").include?("Comedy")}.map do |film| {
                         link: film[0], 
@@ -38,16 +40,23 @@ allfilms.select{ |f| f[5].split(",").include?("Comedy")}.map do |film| {
                         actors: film[9]
                         }
               end.sort_by { |hsh| hsh[:date] }[0...10].each_with_index do |viewfilm, c|
-  print "#{c + 1}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]} [#{viewfilm[:genre]}] #{viewfilm[:actors].chomp}, #{viewfilm[:country]} - #{viewfilm[:duratation]} min). - Link: #{viewfilm[:link]}" 
+  print print "#{c + 1}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]},  #{viewfilm[:genre].split(',').join('/')} - #{viewfilm[:duratation]})." 
   puts
   puts
 end
+
+puts "список всех режиссёров по алфавиту:"
 
 allfilms.map do |film| {director: film[8] 
                         }
               end.uniq.sort_by { |hsh| hsh[:director].split(" ").last }.each_with_index do |viewfilm, c|
 
-  print "#{c + 1}. #{viewfilm[:director].split(" ").last} " 
-  puts
-  puts
+  print "#{c + 1}. #{viewfilm[:director].split(" ").last} ", " " 
+  if (c + 1) % 10 == 0 
+    puts 
+  end
+  
 end
+puts
+print "количество фильмов, снятых не в США: "
+puts allfilms.select{ |f| !f[3].include?("USA")}.count 
