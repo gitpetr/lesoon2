e2ab013a6196e77ba109464a14fc1942924c8ef2
gitpetr <pkhilenko@yandex.ru>
2017-03-25 10:30:59 +0600

    Добавил счетчик и применил мап

--- lesson3/movies.rb
+++ lesson3/movies.rb
@@ -6,17 +6,22 @@ end
 
 allfilms = File.readlines(films)  
 allfilms.map!{|i| i.split("|")}
-
-viewfilms = []
-allfilms.each do |film|
-  viewfilms << Hash[:link, film[0], :name, film[1], :year, film[4], :country, film[3], :genre, film[5], :duratation,  film[6], :rating, film[7], :director, film[8], :actors, film[9]]
-end
  
-counter = 0
-viewfilms.sort_by { |hsh| hsh[:duratation] }.reverse[0...5].each do |viewfilm|
-  counter += 1
-  print "#{counter}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]}, #{viewfilm[:actors][0...-1]+"\\n"}, #{viewfilm[:country]} - #{viewfilm[:duratation]} min). - Link: #{viewfilm[:link]}" 
-  
+allfilms.map! do |film| {
+                        link: film[0], 
+                        name: film[1], 
+                        year: film[4], 
+                        country: film[3], 
+                        genre: film[5], 
+                        duratation:  film[6], 
+                        rating: film[7], 
+                        director: film[8], 
+                        actors: film[9]
+                        }
+              end
+
+allfilms.sort_by { |hsh| hsh[:duratation] }.reverse[0...5].each_with_index do |viewfilm, c|
+  print "#{c + 1}. #{viewfilm[:name]}: #{viewfilm[:director]} (#{viewfilm[:year]}, #{viewfilm[:actors][-1]=="\n"? viewfilm[:actors][0...-1]+"\\n" : viewfilm[:actors]}, #{viewfilm[:country]} - #{viewfilm[:duratation]} min). - Link: #{viewfilm[:link]}" 
   puts
   puts
 end
