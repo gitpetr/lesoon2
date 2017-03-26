bd95391bd7bac83f1a550719a4557cda3e6051ca
gitpetr <pkhilenko@yandex.ru>
2017-03-26 17:43:25 +0600

    исправил мелочи

--- lesson3/movies.rb
+++ lesson3/movies.rb
@@ -1,4 +1,4 @@
-TITRES = [:link, :name, :date, :country, :year, :genre, :duratation, :rating, :director, :actors]
+TITRES = %i[link name date country year genre duratation rating director :actors]
 
 films = ARGV[0] || "../movies.txt"
 def printfilms( view, count )
@@ -12,24 +12,31 @@ end
 
 allfilms = File.readlines(films) 
 allfilms.map!{|i| i.chomp.split("|")}
-puts; puts "\t 5 самых длинных фильмов:"; puts
+puts
+puts "\t 5 самых длинных фильмов:"; puts
 
-allfilms.map { |film| Hash[TITRES.zip(film)] }.sort_by { 
-  |hsh| hsh[:duratation].split(" ")[0].to_i }.reverse[0...5].each_with_index { 
+allfilms.map { |film|  TITRES.zip(film).to_h }.sort_by { 
+  |hsh| hsh[:duratation].split(" ")[0].to_i }.reverse.first(5).each_with_index { 
   |viewfilm, c| printfilms( viewfilm, c )}
 
-puts; puts; puts "\t 10 комедий (первые по дате выхода):"; puts
+puts
+puts
+puts "\t 10 комедий (первые по дате выхода):"; puts
 
-allfilms.select{ |f| f[5].scan(/Comedy/).include?('Comedy')
-                }.map { |film| Hash[TITRES.zip(film)] 
-                }.sort_by { |hsh| hsh[:date] }[0...10].each_with_index {
+allfilms.select{ |f| f[5].include?('Comedy')
+                }.map { |film| TITRES.zip(film).to_h 
+                }.sort_by { |hsh| hsh[:date] }.first(10).each_with_index {
                 |viewfilm, c| printfilms( viewfilm, c )}
 
-puts;  puts;  puts "\t список всех режиссёров по алфавиту:"; puts
+puts
+puts
+puts "\t список всех режиссёров по алфавиту:"; puts
 
 allfilms.map { |film| { director: film[8] }}.uniq.sort_by {|hsh| hsh[:director].split(" ").last 
-              }.each_slice(10){|viewfilm| puts; viewfilm.each {
+              }.each_slice(6){|viewfilm| puts; viewfilm.each {
               |viewfilm| print "\t#{viewfilm[:director].split(" ").last} #{" " * (12 - (viewfilm[:director].split(" ").last.length))}"
               } } 
-puts; puts; print "\tколичество фильмов, снятых не в США: "
+puts
+puts
+print " количество фильмов, снятых не в США: "
 puts allfilms.select{ |f| !f[3].scan(/USA/).include?("USA")}.count; puts 
