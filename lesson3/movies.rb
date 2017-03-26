0f73ccd91e7b088de59477acb496a41b578f3d44
gitpetr <pkhilenko@yandex.ru>
2017-03-26 12:07:57 +0600

    add tab to str

--- lesson3/movies.rb
+++ lesson3/movies.rb
@@ -2,7 +2,7 @@ TITRES = [:link, :name, :date, :country, :year, :genre, :duratation, :rating, :d
 
 films = ARGV[0] || "../movies.txt"
 def printfilms( view, count )
-   puts(" #{count + 1}. #{view[:name]}: #{view[:director]} (#{view[:year]}, \
+   puts("\t#{count + 1}. #{view[:name]}: #{view[:director]} (#{view[:year]}, \
     #{view[:genre].split(',').join('/')} - #{view[:duratation]}).")
 end
 
@@ -12,24 +12,24 @@ end
 
 allfilms = File.readlines(films) 
 allfilms.map!{|i| i.chomp.split("|")}
-puts; puts "  5 самых длинных фильмов:"; puts
+puts; puts "\t 5 самых длинных фильмов:"; puts
 
 allfilms.map { |film| Hash[TITRES.zip(film)] }.sort_by { 
   |hsh| hsh[:duratation].split(" ")[0].to_i }.reverse[0...5].each_with_index { 
   |viewfilm, c| printfilms( viewfilm, c )}
 
-puts; puts; puts "  10 комедий (первые по дате выхода):"; puts
+puts; puts; puts "\t 10 комедий (первые по дате выхода):"; puts
 
 allfilms.select{ |f| f[5].scan(/Comedy/).include?('Comedy')
                 }.map { |film| Hash[TITRES.zip(film)] 
                 }.sort_by { |hsh| hsh[:date] }[0...10].each_with_index {
                 |viewfilm, c| printfilms( viewfilm, c )}
 
-puts;  puts;  puts "  список всех режиссёров по алфавиту:"; puts
+puts;  puts;  puts "\t список всех режиссёров по алфавиту:"; puts
 
 allfilms.map { |film| { director: film[8] }}.uniq.sort_by {|hsh| hsh[:director].split(" ").last 
               }.each_slice(10){|viewfilm| puts; viewfilm.each {
-              |viewfilm| print " #{viewfilm[:director].split(" ").last} #{" " * (12 - (viewfilm[:director].split(" ").last.length))}"
+              |viewfilm| print "\t#{viewfilm[:director].split(" ").last} #{" " * (12 - (viewfilm[:director].split(" ").last.length))}"
               } } 
-puts; puts; print " количество фильмов, снятых не в США: "
+puts; puts; print "\tколичество фильмов, снятых не в США: "
 puts allfilms.select{ |f| !f[3].scan(/USA/).include?("USA")}.count; puts 
