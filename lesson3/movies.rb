3b109b73526996428f5d727f1a19fee147ea0074
gitpetr <pkhilenko@yandex.ru>
2017-03-26 20:19:02 +0600

    full repairing

--- lesson3/movies.rb
+++ lesson3/movies.rb
@@ -1,42 +1,44 @@
-TITRES = %i[link name date country year genre duratation rating director :actors]
+TITRES = %i[link name date country year genre duratation rating director actors]
 
 films = ARGV[0] || "../movies.txt"
 def printfilms( view, count )
    puts("\t#{count + 1}. #{view[:name]}: #{view[:director]} (#{view[:year]}, \
-    #{view[:genre].split(',').join('/')} - #{view[:duratation]}).")
+#{view[:genre].split(',').join('/')} - #{view[:duratation]}).")
 end
 
 unless File.exist?(films)  
   abort " Такого файла не существует"
 end
 
-allfilms = File.readlines(films) 
-allfilms.map!{|i| i.chomp.split("|")}
+allfilms = File.readlines(films).map { |ln| TITRES.zip(ln.chomp.split("|")).to_h }
+director = allfilms.map { |film| film[:director].split(" ").last }.uniq.sort
+ 
 puts
-puts "\t 5 самых длинных фильмов:"; puts
+puts "\t 5 самых длинных фильмов:"
+puts
 
-allfilms.map { |film|  TITRES.zip(film).to_h }.sort_by { 
+allfilms.sort_by { 
   |hsh| hsh[:duratation].split(" ")[0].to_i }.reverse.first(5).each_with_index { 
   |viewfilm, c| printfilms( viewfilm, c )}
 
 puts
 puts
 puts "\t 10 комедий (первые по дате выхода):"; puts
-
-allfilms.select{ |f| f[5].include?('Comedy')
-                }.map { |film| TITRES.zip(film).to_h 
-                }.sort_by { |hsh| hsh[:date] }.first(10).each_with_index {
-                |viewfilm, c| printfilms( viewfilm, c )}
-
+ 
+allfilms.select{ |v| v.to_s.include?('Comedy')  }.sort_by {
+                 |hsh| hsh[:date] }.first(10).each_with_index {
+                 |viewfilm, c| printfilms( viewfilm, c )}     
 puts
 puts
-puts "\t список всех режиссёров по алфавиту:"; puts
-
-allfilms.map { |film| { director: film[8] }}.uniq.sort_by {|hsh| hsh[:director].split(" ").last 
-              }.each_slice(6){|viewfilm| puts; viewfilm.each {
-              |viewfilm| print "\t#{viewfilm[:director].split(" ").last}".ljust(15)
-              } } 
+puts "\t список всех режиссёров по алфавиту:"
 puts
+ 
+director.each_slice(6){|viewfilm|
+                         puts 
+                         viewfilm.each{|name| print "\t#{name}".ljust(15)}}
+
+puts 
 puts
 print " количество фильмов, снятых не в США: "
-puts allfilms.select{ |f| !f[3].scan(/USA/).include?("USA")}.count; puts 
+puts allfilms.select{ |f| !f.to_s.include?("USA")}.count
+puts 
