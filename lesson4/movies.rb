8c50d0e2b1f44468a502a19e7ff4fcbe43ee70c1
gitpetr <pkhilenko@yandex.ru>
2017-03-28 16:04:18 +0600

    add csv

--- lesson4/movies.rb
+++ lesson4/movies.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'ostruct'
 require 'date'
 
-TITRES = %i[link name date country year genre duratation rating director actors]
+TITRES = %i[link name  year country date  genre duratation rating director actors]
 MANTH = %i[Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь]
 
 def printfilms( view, count )
@@ -16,22 +16,13 @@ unless File.exist?(films)
   abort " Такого файла не существует"
 end
 
-allfilms = CSV.read(films, col_sep: '|').map { |ln| TITRES.zip(ln).to_h}.map {|film| OpenStruct.new(film)}
-
+allfilms = CSV.read(films, col_sep: '|', headers: TITRES )
+ 
+allfilms.map {|f|  f.to_h}.map {|film| OpenStruct.new(film)}
+ 
 director = allfilms.map { |film| film[:director].split(" ").last }.uniq.sort
 
-manth = allfilms.sort_by{|f| f[:year][5..6].to_i
-                        }.map { |f| 
-                          if f[:year].length >=7
-                            if f[:year].length == 7
-                              f[:year]+='-22' 
-                            else 
-                              f[:year]
-                            end
-                            Date.parse(f[:year]).strftime("%B")
-                          end
-                           }.compact
-
+manth = allfilms.sort_by{|f| f[:date][5..6].to_i }.map{ |f| Date.parse(f[:date]).strftime("%B") if f[:date].length == 10}.compact
  
 puts
 puts "\t 5 самых длинных фильмов:"
@@ -47,7 +38,7 @@ puts "\t 10 комедий (первые по дате выхода):"
 puts
  
 allfilms.select{ |v| v[:genre].include?('Comedy')  }.sort_by {
-                 |hsh| hsh[:date] }.first(10).each_with_index {
+                 |hsh| hsh[:year] }.first(10).each_with_index {
                  |viewfilm, c| printfilms( viewfilm, c )}     
 puts
 puts
