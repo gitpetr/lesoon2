9aa6809b1ba6e18c18af41f03def0db66b9149b7
gitpetr <pkhilenko@yandex.ru>
2017-04-09 20:45:21 +0600

    mv send(k).class==Array to send(k).is_a? Array

--- lesson5/movie.rb
+++ lesson5/movie.rb
@@ -29,6 +29,6 @@ class Movie
   end
 
   def match_filter?(**option)
-    option.all?{ |k, v|  (self.send(k).class==Array) ? v === self.send(k).join(',') : v === self.send(k) }
+    option.all?{ |k, v|  (self.send(k).is_a?(Array)) ? v === self.send(k).join(',') : v === self.send(k) }
   end
 end
